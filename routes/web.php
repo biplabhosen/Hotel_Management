b8f60630524b8c46e6330ddec9b5515ea30d595b
biplabhosen <biplobhosen214@gmail.com>
2025-12-30 18:28:31 +0600

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
+Route::get('/homeu', function () {
     return view('pages.erp.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
