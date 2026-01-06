4b82fbcc230358a821b209aa5a08518fb585beca
biplabhosen <biplobhosen214@gmail.com>
2026-01-06 07:35:26 +0600

    room type last id

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -25,19 +25,19 @@ class RoomController extends Controller
         print_r($request->all());
         $hotelId = auth()->user()->hotel_id;
 
-        $room = new RoomType();
-        $room->hotel_id = $hotelId;
-        $room->name = $request->room_type_id;
-        $room->bed_type = $request->bed_type;
-        $room->price_per_night = $request->price_per_night;
-        $room->bed_count = $request->bed_count;
-        $room->capacity = $request->capacity;
-        $room_type_id = $room->save();
+        $roomType = new RoomType();
+        $roomType->hotel_id = $hotelId;
+        $roomType->name = $request->room_type_id;
+        $roomType->bed_type = $request->bed_type;
+        $roomType->price_per_night = $request->price_per_night;
+        $roomType->bed_count = $request->bed_count;
+        $roomType->capacity = $request->capacity;
+        $roomType->save();
 
         $room = new Room();
         $room->hotel_id = $hotelId;
         $room->room_number = $request->room_number;
-        $room->room_type_id = $room_type_id;
+        $room->room_type_id = $roomType->id;
         $room->floor = $request->floor;
         $room->status = $request->status;
         $room->save();
